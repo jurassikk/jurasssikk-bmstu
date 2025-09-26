e25e037bde247f8ee9fb97330ddcd2741dbaccf2
jurassikk <u09777ru@gmail.com>
2025-09-26 22:04:23 +0300

    info_lab_1 task2

---
+++ info_lab_1 task2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите количество машин, произведённых на фабрике: ";
+    cin >> n;
+
+    unsigned long long factorial = 1; 
+    int i = 1;
+
+    cout << "\nРост производительности:\n";
+
+    while (i <= n) {
+        factorial *= i;
+        cout << i << "! = " << factorial << endl; 
+        i++;
+    }
+
+    cout << "\nИтого: " << n << "! = " << factorial << endl;
+
+    return 0;
+}
