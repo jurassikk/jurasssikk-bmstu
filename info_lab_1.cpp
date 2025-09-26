bdac8f4b52cca8b7dfb6beec5ac39fee86094be3
jurassikk <u09777ru@gmail.com>
2025-09-26 21:31:17 +0300

    info_lab_1 task1

---
+++ info_lab_1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {    
+    setlocale(LC_ALL, "RU");
+    double speed;
+    cout << "Введите скорость автомобиля (км/ч): ";
+    cin >> speed;
+
+    if (speed < 60) {
+        cout << "Скорость в пределах нормы" << endl;
+    }
+    else if (speed >= 60 && speed <= 80) {
+        cout << "Вы приближаетесь к превышению скорости" << endl;
+    }
+    else {
+        cout << "Превышение скорости!" << endl;
+    }
+
+    return 0;
+}
