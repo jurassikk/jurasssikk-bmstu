39866faa15ddce635092f2c61c067f2428b90656
Yury <u09777ru@gmail.com>
2025-12-01 13:24:34 +0300

    #5 Добавлен ввод данных пользователем

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def find_max(numbers):
     return max(numbers)
 
-print(find_max([1, 5, 3]))
+n = int(input("Введите количество элементов: "))
+numbers = []
+
+for i in range(n):
+    numbers.append(int(input(f"Элемент {i+1}: ")))
+
+print("Максимум:", find_max(numbers))
 
